ead06b7480c97186efc6bcf6bd11175401172ec4
Beniyam Teferra <>
2021-06-04 15:53:33 +0000

    NEW CANDIDATE TESTING

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,83 +1,50 @@
 const input = require('readline-sync');
-console.log("WEL COME TO CANDIDATE-TESTING");
-// TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
 let candidateName;
-candidateName = "Can Twin";
-console.log(("Candidate Name:") , (candidateName));
+let question = "";
+let correctAnswer ="";
+// TODO 2: modify your quiz app to ask 5 questions //
+//Hellow World!
+// TODO 1.1a: Define candidateName // 
+//let candidateName;
+let questions = ["1) Who was the first American woman in space?","2) True or false: 5000 meters = 5 kilometers.","3) (5 + 3)/2 * 10 = ?","4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","5) What is the minimum crew size for the ISS?"]
+
+let correctAnswers = ["Sally Ride","true",40,"Trajectory",3] 
+
+let candidateAnswer = "";
+let candidateAnswers = [];
 //candidateName=input.question("Write Candidate Name");
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-question = "1) Who was the first American woman in space?";
-console.log(question);
-let candidateAnswer;
-candidateAnswer = "Your Answer: sally ride";
-console.log(candidateAnswer);
-let correctAnswer;
-correctAnswer = "Correct Answer: Sally Ride";
-console.log(correctAnswer);
-console.log();
+//TODO 1.2a: Define question, correctAnswer, and candidateAnsw
 
-let questions;
-questions = "2) True or false: 5000 meters = 5 kilometers."
-console.log(questions);
-let candidateAnswers;
-candidateAnswers = "Your Answer: false";
-console.log(candidateAnswers);
-let correctAnswers;
-correctAnswers = "Correct Answer: true";
-console.log(correctAnswers);
-console.log()
-
-let question3;
-question3 = "3) (5 + 3)/2 * 10 = ? "
-console.log(question3);
-let candidateAnswer3;
-candidateAnswer3 = "Your Answer: 45";
-console.log(candidateAnswer3);
-let correctAnswer3;
-correctAnswer3 = "Correct Answer: 40";
-console.log(correctAnswer3);
-console.log()
-
-let question4;
-question4 = `4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?`
-console.log(question4);
-let candidateAnswer4;
-candidateAnswer4 = "Your Answer: trajectory";
-console.log(candidateAnswer4);
-let correctAnswer4;
-correctAnswer4 = "Correct Answer: Trajectory";
-console.log(correctAnswer4);
-console.log()
-
-let question5;
-question5 = "5) What is the minimum crew size for the ISS?"
-console.log(question5);
-let candidateAnswer5;
-candidateAnswer5 = "Your Answer: 10";
-console.log(candidateAnswer5);
-let correctAnswer5;
-correctAnswer5 = "Correct Answer: 3";
-console.log(correctAnswer5);
-console.log()
 
 function askForName() {
+  candidateName = input.question("enter your name?");
+  console.log("Wel Come,"+ candidateName + "!");
   // TODO 1.1b: Ask for candidate's name //
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+for(let i=0; i<questions.length; i++){
+  candidateAnswer = input.question(questions[i]);
+  candidateAnswers.push(candidateAnswer);
 
+}
 
 }
 
 function gradeQuiz(candidateAnswers) {
-
+let numOfCorrect;
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+for(let i=0; i<candidateAnswers.length; i++){
+  if (candidateAnswers[i].toLowerCase()===correctAnswers[i].toLowerCase()){
+    numOfCorrect++; 
+  }
+  console.log(`${questions[i]}`);
+  console.log(`Your answer: ${candidateAnswers[i]}`);
+  console.log(`Correct answer: ${correctAnswers[i]}`);
+}
 
   let grade;
   
